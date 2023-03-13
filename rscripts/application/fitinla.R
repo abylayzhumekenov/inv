63ce894cd495ed1c4aae09990a09704d5c7852b3
Abylay Zhumekenov <abylay.zhumekenov@kaust.edu.sa>
2023-03-13 15:24:07 +0300

    waagh

--- rscripts/application/fitinla.R
+++ rscripts/application/fitinla.R
@@ -2,14 +2,16 @@ library(INLA)
 library(INLAspacetime)
 library(inlabru)
 inla.setOption(smtp = "pardiso", inla.mode = "compact", pardiso.license = "~/pardiso.license")
+# inla.setOption(smtp = "taucs", inla.mode = "compact")
 
 # load the data
 source("getdata.R")
 detach("package:data.table", unload = TRUE)
+wdat = wdat[1:10,1:10]
 
 # create spatial and temporal mesh
 resolution.s = 1000
-resolution.t = 365
+resolution.t = 9
 bound = inla.nonconvex.hull(points = coordinates(stations), 
                             convex = 200, concave = 200, resolution = 100)
 mesh.s = inla.mesh.2d(boundary = bound, 
@@ -35,20 +37,22 @@ data = data.frame(longitude = rep(loc[,1], m.t),
                   time = rep(1:m.t, each = m.s),
                   y = c(as.matrix(wdat[,-1])) / 10,
                   elevation = rep(elevation, m.t),
-                  northing = rep(loc[,2], m.t),
+                  # northing = rep(loc[,2], m.t),
                   sine = rep(sin(2*pi*(1:m.t)/365.25), each = m.s),
                   cosine = rep(cos(2*pi*(1:m.t)/365.25), each = m.s))
 
 # define a model
-model = ~ -1 + Interecept(1) + elevation + northing + 
+model = ~ -1 + Interecept(1) + 
+    # elevation + northing +
     field(list(space = cbind(longitude, latitude),
                time = time),
           model = stmodel)
 stmodel = stModel.define(mesh.s, mesh.t, "121",
                          control.priors = list(prs = c(9.895, 0.0),
-                                               prt = c(14.026, 0.0),
+                                               prt = c(4.026, 0.0),
                                                psigma = c(5.596, 0.0)))
-lkprec = list(prec = list(initial = -1.289, fixed = TRUE))
+# lkprec = list(prec = list(initial = -1.289, fixed = TRUE))
+lkprec = list(prec = list(initial = 10, fixed = TRUE))
 
 # fit the model
 res = bru(model,
@@ -58,7 +62,7 @@ res = bru(model,
                data = data),
           options = list(verbose = TRUE,
                          safe = FALSE,
-                         num.threads = "8:4",
+                         num.threads = "1:1",
                          control.inla = list(int.strategy = "eb"),
                          control.compute = list(config = TRUE)))
 
