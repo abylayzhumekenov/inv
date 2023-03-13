61162f074edf728d42dc6dca23aa00a8c164f5b5
Abylay Zhumekenov <abylay.zhumekenov@kaust.edu.sa>
2023-03-13 17:13:09 +0300

    fix fitinla.R

--- rscripts/application/fitinla.R
+++ rscripts/application/fitinla.R
@@ -2,68 +2,58 @@ library(INLA)
 library(INLAspacetime)
 library(inlabru)
 inla.setOption(smtp = "pardiso", inla.mode = "compact", pardiso.license = "~/pardiso.license")
-# inla.setOption(smtp = "taucs", inla.mode = "compact")
 
 # load the data
 source("getdata.R")
 detach("package:data.table", unload = TRUE)
-wdat = wdat[1:10,1:10]
 
-# create spatial and temporal mesh
-resolution.s = 1000
-resolution.t = 9
-bound = inla.nonconvex.hull(points = coordinates(stations), 
-                            convex = 200, concave = 200, resolution = 100)
-mesh.s = inla.mesh.2d(boundary = bound, 
-                      max.edge = c(1, 2) * resolution.s, 
-                      offset = c(1e-9, resolution.s * 2), 
-                      cutoff = resolution.s / 4)
-mesh.t = inla.mesh.1d(1:resolution.t)
-
-# get dimensions
-wdat = wdat[,1:(resolution.t+1)]
-n.t = mesh.t$n
-n.s = mesh.s$n
-m.t = dim(wdat)[2]-1
+# set dimensions
+m.t = n.t = dim(wdat)[2]-1
 m.s = dim(wdat)[1]
-n.st = n.s * n.t
-m.st = m.s * m.t
+m.st = m.t * m.s
+
+# create mesh
+mesh.t = inla.mesh.1d(1:n.t)
+resolution.s = 200
+bound = inla.nonconvex.hull(points = stations@coords, convex = 200, concave = 200, resolution = 100)
+mesh.s = inla.mesh.2d(boundary = bound, max.edge = c(1, 2)*resolution.s, 
+                      offset = c(1e-3, 3*resolution.s), cutoff = resolution.s/4)
+n.s = mesh.s$n
+n.st = n.t * n.s
 
 # prepare data
+wdat = wdat[1:m.s, 1:(m.t+1)]
 loc = stations@coords[match(wdat$station, stations$station),]
-elevation = stations$elevation[match(wdat$station, stations$station)]
+ele = stations$elevation[match(wdat$station, stations$station)]
 data = data.frame(longitude = rep(loc[,1], m.t),
                   latitude = rep(loc[,2], m.t),
                   time = rep(1:m.t, each = m.s),
                   y = c(as.matrix(wdat[,-1])) / 10,
-                  elevation = rep(elevation, m.t),
-                  # northing = rep(loc[,2], m.t),
-                  sine = rep(sin(2*pi*(1:m.t)/365.25), each = m.s),
-                  cosine = rep(cos(2*pi*(1:m.t)/365.25), each = m.s))
+                  elevation = rep(ele, m.t) - mean(ele),
+                  northing = rep(loc[,2], m.t) - mean(loc[,2]),
+                  harmonic1 = rep(sin(2*pi*(1:m.t-1)/365.25), each = m.s),
+                  harmonic2 = rep(cos(2*pi*(1:m.t-1)/365.25), each = m.s))
 
 # define a model
-model = ~ -1 + Interecept(1) + 
-    # elevation + northing +
-    field(list(space = cbind(longitude, latitude),
-               time = time),
+model = ~ -1 + Intercept(1) + elevation + northing + harmonic1 + harmonic2 +
+    field(list(space = cbind(latitude, longitude), time = time), 
           model = stmodel)
-stmodel = stModel.define(mesh.s, mesh.t, "121",
-                         control.priors = list(prs = c(9.895, 0.0),
-                                               prt = c(4.026, 0.0),
-                                               psigma = c(5.596, 0.0)))
-# lkprec = list(prec = list(initial = -1.289, fixed = TRUE))
-lkprec = list(prec = list(initial = 10, fixed = TRUE))
+stmodel = stModel.define(mesh.s, mesh.t, "121", 
+                         control.priors = list(prs = c(9.895, 0.5),
+                                               prt = c(14.026, 0.5),
+                                               psigma = c(5.596, 0.5)))
+lkprec = list(prec = list(initial = -1.289, fixed = FALSE))
 
 # fit the model
-res = bru(model,
-          like(formula = y ~ .,
-               family = "gaussian",
-               control.family = list(hyper = lkprec),
-               data = data),
-          options = list(verbose = TRUE,
-                         safe = FALSE,
-                         num.threads = "1:1",
-                         control.inla = list(int.strategy = "eb"),
-                         control.compute = list(config = TRUE)))
+result = bru(model, 
+             like(formula = y ~ ., 
+                  family = "gaussian",
+                  control.family = list(hyper = lkprec), 
+                  data = data),
+             options = list(verbose = TRUE,
+                            safe = FALSE,
+                            num.threads = "4:8",
+                            control.inla = list(int.strategy = "eb")))
 
-print(res$summary.hyperpar)
+# print the result
+result$summary.hyperpar
